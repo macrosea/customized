16604506c4b0f7468a7952616814cf5d18289468
macrosea <macrosea@gmail.com>
2017-06-25 18:36:45 +0800

    add http config

---
+++ htpc/install.sh
@@ -0,0 +1,14 @@
+
+d_cur=$(cd "$(dirname $0)"; pwd)
+d_sbin="/usr/local/sbin"
+#udev
+fn_rule="90-usbstorage.rules"
+d_rules="/etc/udev/rules.d"
+[ -f "$d_rules/$fn_rule" ] && rm "$d_rules/$fn_rule"
+cp -f $d_cur/udev/$fn_rule "$d_rules/$fn_rule"
+
+fn_exec="udev4usb-storager"
+[ -L "$d_sbin/$fn_exec" ] && rm "$d_sbin/$fn_exec"
+ln -s $d_cur/udev/$fn_exec "$d_sbin/$fn_exec"
+[ -f "$d_rules/$fn_rule" ] && [ -L "$d_sbin/$fn_exec" ] && echo "Done ^_^"
+
